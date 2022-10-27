785286639417d76394649c95497ad7d7d9480cd4
vphoa34 <vphoa34@gmail.com>
2022-10-27 09:53:51 +0700

    fix migration report table

--- migrations/08-create-reports.js
+++ migrations/08-create-reports.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Reports', {
+    await queryInterface.createTable("Reports", {
       id: {
         allowNull: false,
         primaryKey: true,
@@ -10,12 +10,12 @@ module.exports = {
       },
       Interview_id: {
         allowNull: false,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.STRING(36),
+        type: Sequelize.STRING(36),
+        defaultValue: Sequelize.UUIDV4,
         references: {
           model: "Interviews",
           key: "id",
-        }
+        },
       },
       Interviewer_id: {
         allowNull: false,
@@ -28,15 +28,15 @@ module.exports = {
       },
       mark: {
         allowNull: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       comment: {
         allowNull: true,
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Reports');
-  }
+    await queryInterface.dropTable("Reports");
+  },
 };
