95102c21c217c0e0c4c7eb653212d1b8109aad3e
vphoa34 <vphoa34@gmail.com>
2022-10-31 22:56:13 +0700

    before merging

--- src/dbconnect.js
+++ src/dbconnect.js
@@ -4,7 +4,7 @@
 
 const { Sequelize } = require("sequelize");
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("hrms", "root", "12345", {
+const sequelize = new Sequelize("hrms", "root", "Vpho@342002", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
